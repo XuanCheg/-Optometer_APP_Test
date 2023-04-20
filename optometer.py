a8bf19fae3f54c6c70161bb4358688e5b9f7b4df
XuanCheng <1454421048@qq.com>
2023-04-20 16:04:03 +0800

    添加注释

--- optometer.py
+++ optometer.py
@@ -1,34 +1,39 @@
+# 该类储存了所有有关数据
 class Optometer:
     def __init__(self):
         # 视力表度数列表
         self.eye_chart_new = [4.0, 4.3, 4.5, 4.6, 4.7, 4.9, 5.0, 5.1, 5.2, 5.3]
         self.eye_chart_old = [0.1, 0.2, 0.3, 0.4, 0.5, 0.8, 1.0, 1.2, 1.5, 2.0]
-        # 方向转换
+        # 方向
         self.input_change = {"up": 1, "down": 2, "left": 3, "right": 4, "clear": 5, "unclear": 6}
+        # 根据度数输出图片
         self.Graph = ["1.png", "2.png", "3.png", "4.png"]
         self.Dio = ["4.0\\", "4.3\\", "4.5\\", "4.6\\", "4.7\\", "4.9\\", "5.0\\", "5.1\\", "5.2\\", "5.3\\"]
-        self.data_set = {"correct": 0, "wrong": 0, "flag": 0, "save": [], "count": 0, "diopter": 3, "break_condition": 0}
+        # correct、wrong为对错次数，flag是连续正确次数，save中保存了历次测试对错，count是记录次数，在按钮事件中保证尝试次数小于5次，
+        # diopter是当前度数，break_condition为下次若正确率小于0.6 是否可以输出度数
+        self.data_set = {"correct": 0, "wrong": 0, "flag": 0, "save": [], "count": 0, "diopter": 3,
+                         "break_condition": 0}
 
 
-def check(data: Optometer, random_dir, usr_put: str, data_set: dict):
-    # 判断
+# 判断输入与random产生的方向是否相同
+def check(data: Optometer, random_dir: int, usr_put: str, data_set: dict):
+
     # 视力表随机方向 此方向对应input_change中
+
+    # 当user input为5，6时，即为清晰与否
     if data.input_change[usr_put] == 5:
         data_set["diopter"] += 1
-        data_set["correct"] = 0
-        data_set["wrong"] = 0
-        data_set["flag"] = 0
+        reset(data_set)
     elif data.input_change[usr_put] == 6:
         data_set["diopter"] -= 1
-        data_set["correct"] = 0
-        data_set["wrong"] = 0
-        data_set["flag"] = 0
+        reset(data_set)
         if data_set["diopter"] < 0:
             print("视力低于视力表范围")
     elif data.input_change[usr_put] == random_dir:
         data_set["correct"] += 1
         data_set["save"].append(True)
         data_set["count"] += 1
+        # 判断前后两次是否同为真，若是，则flag+1，若不是，则flag = 1重置
         if len(data_set["save"]) > 1 and data_set["save"][len(data_set["save"]) - 2]:
             data_set["flag"] += 1
         else:
@@ -39,12 +44,13 @@ def check(data: Optometer, random_dir, usr_put: str, data_set: dict):
         data_set["save"].append(False)
 
 
+# 在某些条件下需要重置数据，此函数即为该用途
 def reset(data_set: dict):
     data_set["correct"] = 0
     data_set["wrong"] = 0
     data_set["flag"] = 0
 
-
+# 计算正确率
 def count_rate(data_set: dict):
     if data_set["correct"] + data_set["wrong"] != 0:
         rate = float(data_set["correct"] / (data_set["correct"] + data_set["wrong"]))
--- ui.py
+++ ui.py
@@ -13,12 +13,13 @@ import optometer
 
 data = optometer.Optometer()
 
-
+# 产生随机方向函数
 def randomgraph():
     random_dir = randint(1, 4)
     return random_dir
 
 
+# 为展示图片做准备
 def img_Product(graph: str, dio: str):
     img = QPixmap()
     path = graph + dio
@@ -41,6 +42,7 @@ class Ui_Mainwindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        # 六个按钮，后期声音识别完成后即可删除
         up = QPushButton("up", self)
         up.setGeometry(QtCore.QRect(14, 420, 91, 51))
         down = QPushButton("down", self)
@@ -53,6 +55,7 @@ class Ui_Mainwindow(QMainWindow):
         clear.setGeometry(QtCore.QRect(70, 480, 91, 51))
         unclear = QPushButton("unclear", self)
         unclear.setGeometry(QtCore.QRect(260, 480, 91, 51))
+        # 对错进度条
         self.ProgressBarWrong = QProgressBar(self)
         self.ProgressBarWrong.setGeometry(QtCore.QRect(580, 70, 171, 31))
         self.ProgressBarWrong.setRange(0, 5)
@@ -61,28 +64,33 @@ class Ui_Mainwindow(QMainWindow):
         self.ProgressBarRight.setGeometry(QtCore.QRect(580, 160, 171, 31))
         self.ProgressBarRight.setRange(0, 5)
         self.ProgressBarRight.setValue(0)
+        # 进度条前文字说明
         lbl_wrong = QLabel(self)
         lbl_wrong.setGeometry(QtCore.QRect(500, 70, 71, 31))
         lbl_wrong.setText("WRONG")
         lbl_right = QLabel(self)
         lbl_right.setGeometry(QtCore.QRect(500, 160, 71, 31))
         lbl_right.setText("RIGHT")
+        # 用于输出视力检查结果
         self.textBrowser = QTextBrowser(self)
         self.textBrowser.setGeometry(QtCore.QRect(480, 220, 256, 192))
+        # 视力表图片输出
         self.OptometerShow = QGraphicsView(self)
         self.OptometerShow.setGeometry(QtCore.QRect(0, 0, 421, 411))
         self.scene = QGraphicsScene(self)
         self.scene.setSceneRect(0, 0, 400, 400)
+        # 初次运行时产生随机方向的图片
         self.rand_dir = randomgraph()
         self.scene.addPixmap(img_Product("4.6\\", data.Graph[self.rand_dir - 1]))
         self.OptometerShow.setScene(self.scene)
+        # 按钮按下事件
         up.clicked.connect(self.buttonClicked)
         down.clicked.connect(self.buttonClicked)
         left.clicked.connect(self.buttonClicked)
         right.clicked.connect(self.buttonClicked)
         clear.clicked.connect(self.buttonClicked)
         unclear.clicked.connect(self.buttonClicked)
-
+        # 主界面
         self.statusBar()
         self.resize(800, 600)
         self.center()
@@ -91,29 +99,34 @@ class Ui_Mainwindow(QMainWindow):
 
         self.show()
 
+    # 使得窗口在屏幕中央打开
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    # 重载窗口关闭事件
     def closeEvent(self, event):
 
         reply = QMessageBox.question(self, "消息", "你确定要退出吗？", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if reply == QMessageBox.Yes:
-            event.ignore()
+            event.accept()
         else:
             event.ignore()
 
+    # 按钮事件 若完成声控识别，此函数可删除，更改为一个input事件
     def buttonClicked(self):
         sender = self.sender()
+
         optometer.check(data, self.rand_dir, sender.text(), data.data_set)
+        # 连续三次答对即可进入下一度数
         if data.data_set["flag"] == 3:
             data.data_set["diopter"] += 1
             optometer.reset(data.data_set)
-
+        # 计算正确率
         rate = optometer.count_rate(data.data_set)
-
+        # 五次尝试正确率大于0.6进入下一度数，否则进入上一度数，如果先大于0.6再小于0.6，则显示目前度数
         if rate > 0.6 and data.data_set["count"] % 5 == 0:
             data.data_set["diopter"] += 1
             optometer.reset(data.data_set)
@@ -128,6 +141,7 @@ class Ui_Mainwindow(QMainWindow):
                 self.textBrowser.setText(text)
                 return
             optometer.reset(data.data_set)
+        # 更新进度条和随机图片
         self.ProgressBarWrong.setValue(data.data_set["wrong"])
         self.ProgressBarRight.setValue(data.data_set["correct"])
         self.rand_dir = randomgraph()
